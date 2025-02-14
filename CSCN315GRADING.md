b0688fe11dbb41f63ce837f1a8331f7dfafb2d4e
Jeffreyman101 <17098923+jeffreyv101@users.noreply.github.com>
2025-02-14 13:33:29 -0500

    Individual Case Project 3

--- CSCN315GRADING.md
+++ CSCN315GRADING.md
@@ -1,21 +1,7 @@
 # CSCN 315 Semester Project Explanation
-## Individual Case Project 2
-1. Create a Form to Gather User Data
-    - [contact.html](https://jeffreyv101.github.io/sdc-club-website/contact.html)
+## Individual Case Project 3
+1. Create external JS file named proj3.js
 
-2. Perform Calculations Based on User Input
-    - [contact.html](https://jeffreyv101.github.io/sdc-club-website/contact.html)
+2. Create a JavaScript program that processes data of your choice and determines and displays relevant messages based on multiple conditions. The program should include <code>if, else, else if</code> statements, a <code>switch</code> statement, and an <code>array</code> to manage the data. 
 
-3. Validate User Input
-    - [contact.html](https://jeffreyv101.github.io/sdc-club-website/contact.html)
-
-4. Display Results Back to User
-    - [contact.html](https://jeffreyv101.github.io/sdc-club-website/contact.html)
-
-5. Log Data to the Console
-    - [contact.html](https://jeffreyv101.github.io/sdc-club-website/contact.html)
-    - [proj2.js](./proj2.js)
-
-6. Event Handling
-    - [contact.html](https://jeffreyv101.github.io/sdc-club-website/contact.html)
-    - [proj2.js](./proj2.js)
+3. Calculate additional data using a <code>for</code> loop.
---
+++ proj3.js
@@ -0,0 +1,66 @@
+let tasks = [];
+
+
+function addTask() 
+{
+    event.preventDefault();
+    let taskToAdd = document.getElementById("task").value;
+    let priorityLevel = document.getElementById("priorityLevel").value;
+
+    if (taskToAdd == "")
+    {
+        alert("Please enter a task label");
+    }
+    else
+    {
+        let taskID = tasks.length;
+        tasks.push(priorityLevel);
+        tasks.push([taskID, taskToAdd]);
+        tasks.push("<button onclick='removeTask(" + (taskID) + ")'>Delete</button>");
+
+        displayTasks();
+        taskToAdd = "";
+    }
+}
+
+function removeTask(taskID) {
+    for (let i = 1; i < tasks.length; i += 3) {
+        if (tasks[i][0] == taskID) {
+            tasks.splice(i - 1, 3);
+            break;
+        }
+    }
+    displayTasks();
+}
+
+function displayTasks() {
+    let taskDiv = document.getElementById("tasks");
+    
+    taskDiv.innerHTML = "";
+    
+    // If there are tasks to display
+    if (tasks.length == 0) {
+        taskDiv.innerHTML = "<p>No tasks to display</p>";
+    }
+    else {
+        taskDiv.innerHTML += "<h3>Tasks</h3>";
+        for (let i = 0; i < tasks.length; i += 3) {
+            switch (tasks[i]) {
+                case "low":
+                    taskDiv.innerHTML += "<p id='taskLabel'><b style='color: green'>" + tasks[i+1][1] + "</b></p>";
+                    break;
+                case "medium":
+                    taskDiv.innerHTML += "<p id='taskLabel'><b style='color: orange'>" + tasks[i+1][1] + "</b></p>";
+                    break;
+                case "high":
+                    taskDiv.innerHTML += "<p id='taskLabel'><b style='color: red'>" + tasks[i+1][1] + "</b></p>";
+                    break;
+                default:
+                    taskDiv.innerHTML += "<p id='taskLabel'><b>" + tasks[i+1][1] + "</b></p>";
+                    break;
+            }
+            taskDiv.innerHTML += "<p id='taskDelete'>" + tasks[i + 2] + "</p>";
+            
+        }
+    }
+}
