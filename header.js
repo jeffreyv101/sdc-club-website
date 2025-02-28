a3e6a103eb8e8541e17dbde982ef1edd309febab
Jeffreyman101 <17098923+jeffreyv101@users.noreply.github.com>
2025-02-28 17:49:01 -0500

    Mobile UI Improvements

--- header.js
+++ header.js
@@ -1,8 +1,11 @@
 window.addEventListener('resize', function() {
     const clubNameElement = document.getElementById('clubName');
+    const projectTitle = document.getElementById('projectTitle');
     if (window.innerWidth < 552) {
+        projectTitle.textContent = 'Projects';
         clubNameElement.textContent = 'SDC';
     } else {
+        projectTitle.textContent = 'Current Projects';
         clubNameElement.textContent = 'Software Development Club';
     }
 });
@@ -10,9 +13,12 @@ window.addEventListener('resize', function() {
 // Initial check when the page loads
 document.addEventListener('DOMContentLoaded', function() {
     const clubNameElement = document.getElementById('clubName');
+    const projectTitle = document.getElementById('projectTitle');
     if (window.innerWidth < 552) {
+        projectTitle.textContent = 'Projects';
         clubNameElement.textContent = 'SDC';
     } else {
+        projectTitle.textContent = 'Current Projects';
         clubNameElement.textContent = 'Software Development Club';
     }
 });
